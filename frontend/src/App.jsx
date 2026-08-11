2127d3728957086efa4134758b9602dd4943706b
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-08-11 15:56:24 +0100

    feat: implement ProtectedRoute component for route protection based on authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,27 +13,32 @@ import FarmDetail from './pages/farmDetails';
 import News from './pages/news';
 import CropDetail from './pages/cropDetail';
 import LivestockDetail from './pages/livestockDetail';
-
+import ProtectedRoute from './components/protectRoutes';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/farms" element={<MyFarms />} />
-      <Route path="/crops" element={<Crops />} />
-      <Route path="/livestock" element={<Livestock />} />
-      <Route path="/diagnose" element={<Diagnose />} />
-      <Route path="/advisory" element={<Advisory />} />
-      <Route path="/askAIAssistant" element={<Chat />} />
-      <Route path="/comingSoon" element={<ComingSoon />} />
+      {/* Public routes */}
       <Route path="/auth" element={<Auth />} />
-      <Route path="/farms/:id" element={<FarmDetail />} />
       <Route path="/news" element={<News />} />
-      <Route path="/crops/:id" element={<CropDetail />} />
-      <Route path="/livestock/:id" element={<LivestockDetail />} />
+      <Route path="/comingSoon" element={<ComingSoon />} />
+
+      {/* Protected routes */}
+      <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/farms" element={<MyFarms />} />
+        <Route path="/farms/:id" element={<FarmDetail />} />
+        <Route path="/crops" element={<Crops />} />
+        <Route path="/crops/:id" element={<CropDetail />} />
+        <Route path="/livestock" element={<Livestock />} />
+        <Route path="/livestock/:id" element={<LivestockDetail />} />
+        <Route path="/diagnose" element={<Diagnose />} />
+        <Route path="/advisory" element={<Advisory />} />
+        <Route path="/askAIAssistant" element={<Chat />} />
+      </Route>
     </Routes>
   );
-}
+};
 
 export default App;
 
